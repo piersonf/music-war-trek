59a6a1fda9d49dbb048ba3e81f7989efe19de085
Charlie Dwyer <cdwr17@gmail.com>
2025-06-20 15:01:18 -0400

    Improve artist display script

--- scripts/artistDetails.js
+++ scripts/artistDetails.js
@@ -3,8 +3,39 @@ import { artists } from '/data/artists.js';
 const urlParams = new URLSearchParams(window.location.search);
 const artistId = urlParams.get('id');
 console.log(`getting ${artistId}`);
+
 const thisArtist = artists.find((artist) => artist.artistId === artistId);
-console.log(thisArtist);
+
+if (thisArtist) {
+  const {
+    artistName,
+    artistDescription,
+    artistStrength,
+    artistCharisma,
+    artistAlignment,
+    artistIntelligence,
+    artistSpecialPowers
+  } = thisArtist;
+
+  const artistNameElement = document.getElementById('artistName');
+  const artistDescriptionElement = document.getElementById('artistDescription');
+  const artistStrengthElement = document.getElementById('artistStrength');
+  const artistCharismaElement = document.getElementById('artistCharisma');
+  const artistAlignmentElement = document.getElementById('artistAlignment');
+  const artistIntelligenceElement = document.getElementById('artistIntelligence');
+  const artistSpecialPowersElement = document.getElementById('artistSpecialPowers');
+
+  artistNameElement.innerHTML = artistName;
+  artistDescriptionElement.innerHTML = artistDescription;
+  artistStrengthElement.innerHTML = artistStrength;
+  artistCharismaElement.innerHTML = artistCharisma;
+  artistAlignmentElement.innerHTML = artistAlignment;
+  artistIntelligenceElement.innerHTML = artistIntelligence;
+  artistSpecialPowersElement.innerHTML = artistSpecialPowers;
+} else {
+  console.error('Artist not found in database');
+}
+/*console.log(thisArtist);
 document.getElementById('artistName').innerHTML = thisArtist.artistName;
 document.getElementById('artistDescription').innerHTML =
   thisArtist.artistDescription;
@@ -15,4 +46,4 @@ document.getElementById('artistAlignment').innerHTML =
 document.getElementById('artistIntelligence').innerHTML =
   thisArtist.artistIntelligence;
 document.getElementById('artistSpecialPowers').innerHTML =
-  thisArtist.artistSpecialPowers;
+  thisArtist.artistSpecialPowers;*/
